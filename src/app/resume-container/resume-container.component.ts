db5f841d2232a1e1c8863c8d03ec017b9cdcf488
Radoslav Ivanov <riivanov63@gmail.com>
2022-07-27 20:28:59 +0300

    create new resume-container component

---
+++ src/app/resume-container/resume-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-container',
+  templateUrl: './resume-container.component.html',
+  styleUrls: ['./resume-container.component.css']
+})
+export class ResumeContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
