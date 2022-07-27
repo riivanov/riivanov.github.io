627cdb7a91573b99fe95bcb775ac4644c10f2159
Radoslav Ivanov <riivanov63@gmail.com>
2022-07-27 20:27:42 +0300

    seems unneeded on localhost

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+  // if (window['ngRef']) {
+  //   window['ngRef'].destroy();
+  // }
+  // window['ngRef'] = ref;
 
   // Otherwise, log the boot error
 }).catch(err => console.error(err));
