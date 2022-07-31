8019b612cc743f79189ae3b8c69b08857a402a8b
Radoslav Ivanov <riivanov63@gmail.com>
2022-08-01 02:34:29 +0300

    removed resume-container in favor of resume-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { ResumeContainerComponent } from './resume/resume-container/resume-container.component';
 import { ResumeLeftPaneComponent } from './resume/resume-left-pane/resume-left-pane.component';
 import { ResumeMainPaneComponent } from './resume/resume-main-pane/resume-main-pane.component';
-import { ResumePageComponent } from './resume/resume-page/resume-page.component';
 import { AppComponent } from './app.component';
+import { ResumePageComponent } from './resume/resume-page/resume-page.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ 
-    ResumeContainerComponent, 
     ResumeLeftPaneComponent, 
     ResumeMainPaneComponent, 
     ResumePageComponent, 
