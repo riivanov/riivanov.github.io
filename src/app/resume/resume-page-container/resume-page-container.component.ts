0f30f57c19366fab5790d5346d783c3415c5abe8
Radoslav Ivanov <riivanov63@gmail.com>
2022-08-07 17:04:00 +0300

    resume page container component

---
+++ src/app/resume/resume-page-container/resume-page-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'resume-page-container',
+  templateUrl: './resume-page-container.component.html',
+  styleUrls: ['./resume-page-container.component.scss']
+})
+export class ResumePageContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/resume/resume.module.ts
+++ src/app/resume/resume.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { ResumeLeftPaneComponent } from './resume-left-pane/resume-left-pane.component';
 import { ResumeMainPaneComponent } from './resume-main-pane/resume-main-pane.component';
 import { ResumePageComponent } from './resume-page/resume-page.component';
+import { ResumePageContainerComponent } from './resume-page-container/resume-page-container.component';
 
 @NgModule({
   imports: [
@@ -13,8 +14,9 @@ import { ResumePageComponent } from './resume-page/resume-page.component';
     ResumeLeftPaneComponent, 
     ResumeMainPaneComponent, 
     ResumePageComponent, 
-    AboutComponent
+    AboutComponent, 
+    ResumePageContainerComponent
   ],
-  exports: [ ResumePageComponent ]
+  exports: [ ResumePageContainerComponent ]
 })
 export class ResumeModule { }
